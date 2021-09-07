9706c474f70930f21f99fe0ba496c119ce231bbf
ahkamul <sasraishougat@gmail.com>
2021-09-07 11:05:44 +0600

    Some codes on Objects Definitions from w3school added to index.js

--- index.js
+++ index.js
@@ -27,19 +27,49 @@
 
 // Stack Learner YT 053 : Setting Object Properties in JS
 
-var point = {
-    x : 10, 
-    y : 20,
-    z : 15
-}
+// var point = {
+//     x : 10, 
+//     y : 20,
+//     z : 15
+// }
 
-point.x = 45
-point.a = 16
+// point.x = 45
+// point.a = 16
+
+// // console.log(point)
+// // console.log(point.d)
+
+// var prop = 'z'
+// point [prop] = 55 
 
 // console.log(point)
-// console.log(point.d)
 
-var prop = 'z'
-point [prop] = 55 
 
-console.log(point)
+// Studying JS Object from w3school: 
+
+// Object Definitions:
+
+// var person = {
+//     firstName : "John",
+//     lastName : "Doe",
+//     age : 50,
+//     eyeColor : "blue"
+// }
+// console.log(person);
+
+// var person = {}
+// person.firstName = "John";
+// person.lastName = "Doe";
+// person.age = 50;
+// person.eyeColor = "blue";
+
+// console.log(person);
+
+var person = new Object();
+
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue";
+
+console.log(person);
